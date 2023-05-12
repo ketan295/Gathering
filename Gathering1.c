3b3fd4b2eb4928cd8d0197293401a223a5940898
ketan295 <130463504+ketan295@users.noreply.github.com>
2023-05-12 17:15:15 +0530

    Add files via upload

---
+++ Gathering1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+fact(int num)
+{
+	if(num<=0)
+	{
+		return 1;
+	}
+	else 
+	{
+		num=num*fact(num-1);
+	}
+	return num;
+}
+
+main()
+{
+	int n;
+	printf("Enter value of n:");
+	scanf("%d",&n);
+	
+	fact(n);
+	printf("%d!=%d",n,fact(n));
+	
+}
